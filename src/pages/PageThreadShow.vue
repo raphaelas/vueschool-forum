1daea570b25628e1c2a7eeaafa918ae6c0ccb01d
Raphael Astrow <raphaelastrow@zoho.com>
2021-07-24 11:03:34 -0400

    Global base components

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,35 +1,27 @@
 <template>
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
-    <div class="post-list">
-      <div v-for="postId in thread.posts"
-           class="post"
-      >
-        <div class="user-info">
-          <a href="#" class="user-name">{{ users[posts[postId].userId].name }}</a>
-          <a href="#">
-            <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-        </div>
-        <div class="post-content">
-          <div>
-            {{ posts[postId].text }}
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{ posts[postId].publishedAt }}
-        </div>
-      </div>
-    </div>
+    <p>
+      By <a href="#" class="link-unstyled">Robin</a>, <AppDate :timestamp="thread.publishedAt" />.
+      <span style="float:right; margin-top: 2px;" class="hide-mobile text-faded text-small">3 replies by 3 contributors</span>
+    </p>
+    <PostList :posts="posts" />
+    <PostEditor 
+      @save="addPost"
+      :threadId="id"
+    />
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
+import PostList from '@/components/PostList'
+import PostEditor from '@/components/PostEditor'
 export default {
+  components: {
+    PostList,
+    PostEditor
+  },
   props: {
     id: {
       required: true,
@@ -38,9 +30,22 @@ export default {
   },
   data () {
     return {
-      thread: sourceData.threads[this.id],
-      posts: sourceData.posts,
-      users: sourceData.users
+      thread: sourceData.threads[this.id]
+    }
+  },
+  computed: {
+    posts () {
+      const postIds = Object.values(this.thread.posts)
+      return Object.values(sourceData.posts)
+        .filter(post => postIds.includes(post['.key']))
+    }
+  },
+  methods: {
+    addPost ({post}) {
+      const postId = post['.key']
+      this.$set(sourceData.posts, postId, post)
+      this.$set(this.thread.posts, postId, postId)
+      this.$set(sourceData.users[post.userId].posts, postId, postId)
     }
   }
 }
